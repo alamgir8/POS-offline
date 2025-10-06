e275fa07fa6be4c66c1d918be68fa2bf9f7caa4e
alamgir8 <ahossain38900@gmail.com>
2025-10-06 15:56:54 +0600

    feat(v-1): updated

--- start.sh
+++ start.sh
@@ -20,4 +20,23 @@ remote:      https://github.com/alamgir8/POS-offline/security/dependabot[K
 remote: 
 To https://github.com/alamgir8/POS-offline.git
    1feb2b1..4d7ac48  main -> main
-[1m[7m%[27m[1m[0m                                                                                                                               [0m[27m[24m[Jalamgirhossain@Alamgirs-MacBook-Pro offline-pos % [K[?2004h
+[1m[7m%[27m[1m[0m                                                                                                                               [0m[27m[24m[Jalamgirhossain@Alamgirs-MacBook-Pro offline-pos % [K[?2004hgit pushcommit -m"feat(v-1): added raspberry pi configuration doc"[58Dadd .                                                     [53D[?2004l
+[1m[7m%[27m[1m[0m                                                                                                                               [0m[27m[24m[Jalamgirhossain@Alamgirs-MacBook-Pro offline-pos % [K[?2004hgit add .push commit -m"feat(v-1): added raspberry pi configuration doc"[?2004l
+[main 65c6459] feat(v-1): added raspberry pi configuration doc
+ 2 files changed, 315 insertions(+), 91 deletions(-)
+[1m[7m%[27m[1m[0m                                                                                                                               [0m[27m[24m[Jalamgirhossain@Alamgirs-MacBook-Pro offline-pos % [K[?2004hgit commit -m"feat(v-1): added raspberry pi configuration doc"[58Dadd .                                                     [53Dpush [?2004l
+Enumerating objects: 7, done.
+Counting objects:  14% (1/7)Counting objects:  28% (2/7)Counting objects:  42% (3/7)Counting objects:  57% (4/7)Counting objects:  71% (5/7)Counting objects:  85% (6/7)Counting objects: 100% (7/7)Counting objects: 100% (7/7), done.
+Delta compression using up to 12 threads
+Compressing objects:  25% (1/4)Compressing objects:  50% (2/4)Compressing objects:  75% (3/4)Compressing objects: 100% (4/4)Compressing objects: 100% (4/4), done.
+Writing objects:  25% (1/4)Writing objects:  50% (2/4)Writing objects:  75% (3/4)Writing objects: 100% (4/4)Writing objects: 100% (4/4), 4.68 KiB | 4.68 MiB/s, done.
+Total 4 (delta 1), reused 0 (delta 0), pack-reused 0 (from 0)
+remote: Resolving deltas:   0% (0/1)[Kremote: Resolving deltas: 100% (1/1)[Kremote: Resolving deltas: 100% (1/1), completed with 1 local object.[K
+remote: 
+remote: GitHub found 2 vulnerabilities on alamgir8/POS-offline's default branch (1 high, 1 moderate). To find out more, visit:[K
+remote:      https://github.com/alamgir8/POS-offline/security/dependabot[K
+remote: 
+To https://github.com/alamgir8/POS-offline.git
+   4d7ac48..65c6459  main -> main
+[1m[7m%[27m[1m[0m                                                                                                                               [0m[27m[24m[Jalamgirhossain@Alamgirs-MacBook-Pro offline-pos % [K[?2004hcclear[?2004l
+[3J[H[2J[1m[7m%[27m[1m[0m                                                                                                                               [0m[27m[24m[Jalamgirhossain@Alamgirs-MacBook-Pro offline-pos % [K[?2004hnnpm ru    n  ggit add
